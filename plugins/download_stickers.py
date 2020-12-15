76ebcf08e6f770aaba8bb60453e28d1024106a1d
ShinchanNohara1 <72728580+ShinchanNohara1@users.noreply.github.com>
2020-12-15 13:15:28 +0530

    Update download_stickers.py

--- plugins/download_stickers.py
+++ plugins/download_stickers.py
@@ -31,7 +31,7 @@ from helper_funcs.display_progress import progress_for_pyrogram
 async def DownloadStickersBot(bot, update):
     TRChatBase(update.from_user.id, update.text, "DownloadStickersBot")
     if str(update.from_user.id) in Config.BANNED_USERS:
-        bot.edit_message_text(
+        await bot.edit_message_text(
             chat_id=update.message.chat.id,
             text=Translation.ABUSIVE_USERS,
             message_id=update.message.message_id,
@@ -39,73 +39,62 @@ async def DownloadStickersBot(bot, update):
             parse_mode=pyrogram.ParseMode.HTML
         )
         return
-    if update.sticker.is_animated:
-        await update.delete()
-        return
-
-    LOGGER.info(update.from_user)
-    download_location = os.path.join(
-        DOWNLOAD_LOCATION,
-        (
-            str(update.from_user.id),
-            "_DownloadStickersBot_",
-            str(update.from_user.id),
-            ".png"
-        )
-    )
-    a = await update.reply_text(
-        text=Translation.DOWNLOAD_START
+    logger.info(update.from_user)
+    download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "_DownloadStickersBot_" + str(update.from_user.id) + ".png"
+    a = await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.DOWNLOAD_START,
+        reply_to_message_id=update.message_id
     )
     try:
         c_time = time.time()
-        the_real_download_location = await update.download(
+        the_real_download_location = await bot.download_media(
+            message=update,
             file_name=download_location,
             progress=progress_for_pyrogram,
-            progress_args=(
-                Translation.DOWNLOAD_START,
-                a,
-                c_time
-            )
+            progress_args=(Translation.DOWNLOAD_START, a.message_id, update.chat.id, c_time)
         )
     except (ValueError) as e:
-        await a.edit_text(
-            text=str(e)
+        await bot.edit_message_text(
+            text=str(e),
+            chat_id=update.chat.id,
+            message_id=a.message_id
         )
         return False
-    await a.edit_text(
-        text=Translation.SAVED_RECVD_DOC_FILE
+    await bot.edit_message_text(
+        text=Translation.SAVED_RECVD_DOC_FILE,
+        chat_id=update.chat.id,
+        message_id=a.message_id
     )
-    # https://stackoverflow.com/a/21669827/4723940
-    Image.open(the_real_download_location).convert(
-        "RGB"
-    ).save(the_real_download_location)
-    #
     c_time = time.time()
-    await a.reply_document(
+    await bot.send_document(
+        chat_id=update.chat.id,
         document=the_real_download_location,
         # thumb=thumb_image_path,
         # caption=description,
         # reply_markup=reply_markup,
-        # reply_to_message_id=a.message_id,
+        reply_to_message_id=a.message_id,
         progress=progress_for_pyrogram,
         progress_args=(
-            Translation.UPLOAD_START,
-            a,
-            c_time
+            Translation.UPLOAD_START, a.message_id, update.chat.id, c_time
         )
     )
-    await a.reply_photo(
+    await bot.send_photo(
+        chat_id=update.chat.id,
         photo=the_real_download_location,
         # thumb=thumb_image_path,
         # caption=description,
         # reply_markup=reply_markup,
-        # reply_to_message_id=a.message_id,
+        reply_to_message_id=a.message_id,
         progress=progress_for_pyrogram,
         progress_args=(
-            Translation.UPLOAD_START,
-            a,
-            c_time
+            Translation.UPLOAD_START, a.message_id, update.chat.id, c_time
         )
     )
     os.remove(the_real_download_location)
-    await a.delete()
+    await bot.edit_message_text(
+        text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
+        chat_id=update.chat.id,
+        message_id=a.message_id,
+        disable_web_page_preview=True
+    )
