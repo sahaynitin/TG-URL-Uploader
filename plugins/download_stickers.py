23a4633dc4706f078b33f0b05cfc424ba00c8fe3
ShinchanNohara1 <72728580+ShinchanNohara1@users.noreply.github.com>
2020-12-15 13:39:35 +0530

    Update download_stickers.py

--- plugins/download_stickers.py
+++ plugins/download_stickers.py
@@ -92,9 +92,4 @@ async def DownloadStickersBot(bot, update):
         )
     )
     os.remove(the_real_download_location)
-    bot.edit_message_text(
-        text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
-        chat_id=update.chat.id,
-        message_id=a.message_id,
-        disable_web_page_preview=True
-    )
+    await a.delete()
